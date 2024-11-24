f7003161e3d5eb948e6eba250055db0c1cb6669b
J O S E P H <maks.khudenkikh@mail.ru>
2024-11-24 14:32:15 +0300

    Test Init

---
+++ Assets/Project/Scripts/Rotationbruh.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts
+{
+    public class Rotationbruh : MonoBehaviour
+    {
+        public float rotationSpeed = 100f;
+
+        void Update()
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
