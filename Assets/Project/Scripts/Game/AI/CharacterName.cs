73a2ccea200d4005c1a222f6f30181630a5f4c8c
J O S E P H <maks.khudenkikh@mail.ru>
2024-12-30 17:08:46 +0300

    charname add

---
+++ Assets/Project/Scripts/Game/AI/CharacterName.cs
@@ -0,0 +1,21 @@
+﻿using Project.Scripts.Player;
+using UnityEngine;
+using Zenject;
+
+namespace Project.Scripts.Game.AI
+{
+    public class CharacterName : MonoBehaviour
+    {
+        [Inject] private PlayerController _playerController;
+
+        private void FixedUpdate()
+        {
+            Vector3 direction = _playerController.transform.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+        
+            // Поворачиваем текст, добавляя 180 градусов по оси Y
+            transform.rotation = Quaternion.Euler(0, rotation.eulerAngles.y + 180, 0);
+            
+        }
+    }
+}
